93dd08493962617b458a600153468cf42d6a4830
Macfield <rladudals02@naver.com>
2017-01-25 21:19:44 +0900

    Yellow ID API basic form

---
+++ app.js
@@ -0,0 +1,93 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+
+var bodyParser = require('body-parser');
+
+var config = require('config.json')('./config/config.json');
+
+var fs = require('fs');
+
+var app = express();
+
+var morgan = require('morgan');
+
+var logDirectory = './log';
+
+// ensure log directory exists
+if (!fs.existsSync(logDirectory)){
+    fs.mkdirSync(logDirectory);
+}
+
+var FileStreamRotator = require('file-stream-rotator');
+
+// create a rotating write stream
+var accessLogStream = FileStreamRotator.getStream({
+    date_format: 'YYYYMMDD',
+    filename: logDirectory + '/access-%DATE%.log',
+    frequency: 'daily',
+    verbose: false
+});
+
+
+// setup the logger
+//app.use( morgan('combined', {stream: accessLogStream}));
+app.use( morgan('dev', {stream: accessLogStream}));
+
+
+// routes module
+var msg_yellow_id = require('./routes/yellow_id/yellow_id');
+
+
+//console.log("My webpage start!");
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// uncomment after placing your favicon in /public
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'dist')));
+
+
+
+// Yellow ID API server
+app.use('/yellow_id', msg_yellow_id);
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+module.exports = app;
